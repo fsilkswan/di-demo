787c4cf0f46784b3bc50c0468d2bbee318c6bcba
fseidenschwan <ferdinand@seidenschwan.de>
2018-12-12 23:33:25 +0100

    Refactoring: Moved custom properties to "application.properties" file.

--- src/main/java/guru/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/guru/springframework/didemo/config/PropertyConfig.java
@@ -1,32 +1,15 @@
 package guru.springframework.didemo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.env.Environment;
 
 import guru.springframework.didemo.examplebeans.FakeDataSource;
 import guru.springframework.didemo.examplebeans.FakeJmsBroker;
 
 @Configuration
-// @PropertySource({ "classpath:datasource.properties", "classpath:jmsbroker.properties" })
-@PropertySources({
-                   @PropertySource("classpath:datasource.properties"),
-                   @PropertySource("classpath:jmsbroker.properties")
-})
 public class PropertyConfig
 {
-    // The following bean is provided in Spring Boot environment by auto-configuration. In classical Spring environment it is required to be created like this:
-    // @Bean
-    // public static PropertySourcesPlaceholderConfigurer properties()
-    // {
-    // final PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-    // return propertySourcesPlaceholderConfigurer;
-    // }
-
     @Value("${guru.password}")
     String dataSourcePassword;
 
@@ -35,9 +18,6 @@ public class PropertyConfig
 
     @Value("${guru.username}")
     String dataSourceUsername;
-
-    @Autowired
-    Environment env;
 
     @Value("${guru.jms.password}")
     String jmsBrokerPassword;
@@ -53,7 +33,6 @@ public class PropertyConfig
     {
         final FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUsername(dataSourceUsername);
-        fakeDataSource.setUsername(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(dataSourcePassword);
         fakeDataSource.setDatabaseUrl(dataSourceUrl);
 
